30f1b3842dca6df95fa0415afc6fdfa313e9d27a
devudilip <devu.dilip@gmail.com>
2014-05-26 13:48:56 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Continuos::Application.config.secret_key_base = 'f8a201c9fc4a7089aaa0e22d395115dc1e43b3ff2bee6f8ed72e97e0372b877d6d277269ad57bd7b1e8a3bd851c1c27bd59c7b15c6bdfa9a968b7d2d51f20952'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Continuos::Application.config.session_store :cookie_store, key: '_continuos_session'
