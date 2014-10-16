fd42aa89724ffe8f70da056ba196475188239e0c
devudilip <devu.dilip@gmail.com>
2014-10-16 16:25:05 +0530

    .gitignore is now working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Continuos::Application.routes.draw do
+
+  root 'users#index'
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
