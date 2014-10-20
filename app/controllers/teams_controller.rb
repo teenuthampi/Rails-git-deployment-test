b0a46605a2868a605b649262654f729962ba4735
Divya <divya@carmatec.com>
2014-10-20 16:39:46 +0530

    search functionality added

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,8 +4,16 @@ class TeamsController < ApplicationController
   # GET /teams
   # GET /teams.json
   def index
-    @teams = Team.paginate(:page => params[:page], :per_page => 3)
+
+    if params[:search]
+         @teams = Team.search(params[:search]).paginate(:page => params[:page], :per_page => 3)
+      else
+        @teams = Team.paginate(:page => params[:page], :per_page => 3)
+      end
+
   end
+
+
 
   # GET /teams/1
   # GET /teams/1.json
